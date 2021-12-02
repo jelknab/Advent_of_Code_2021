2f09c78793fc21fff96f597e8e6dd48e334cd461
Jelle Metzlar <jelle.metzlar@outlook.com>
2021-12-02 09:29:56 +0100

    Completed day 2

---
+++ Advent of Code 2021/Day 2/Day2.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Advent_of_Code_2021.Day_2
+{
+    public class Day2: IDay
+    {
+        public IEnumerable<string> ReadCommandInput()
+        {
+            return File.ReadLines("Day 2/input");
+        }
+        
+        public static IEnumerable<(string command, int value)> ReadCommands(IEnumerable<string> input)
+        {
+            return input
+                .Select(i => i.Split(' '))
+                .Select(commandParts => (command: commandParts[0], value: int.Parse(commandParts[1])));
+        }
+        
+        public static (int x, int y) RunCommandsPart1(IEnumerable<(string command, int value)> commands)
+        {
+            var x = 0;
+            var y = 0;
+            foreach (var (command, value) in commands)
+            {
+                switch (command)
+                {
+                    case "forward":
+                        x += value;
+                        break;
+                    case "down":
+                        y += value;
+                        break;
+                    case "up":
+                        y -= value;
+                        break;
+                    
+                    default:
+                        throw new Exception($"Unknown command {command}");
+                        
+                }
+            }
+
+            return (x, y);
+        }
+        
+        public void PrintSolution1()
+        {
+            var (x, y) = RunCommandsPart1(ReadCommands(ReadCommandInput()));
+            Console.WriteLine(x * y);
+        }
+
+        public static (int x, int y) RunCommandsPart2(IEnumerable<(string command, int value)> commands)
+        {
+            var x = 0;
+            var y = 0;
+            var aim = 0;
+            foreach (var (command, value) in commands)
+            {
+                switch (command)
+                {
+                    case "forward":
+                        x += value;
+                        y += value * aim;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                    
+                    default:
+                        throw new Exception($"Unknown command {command}");
+                }
+            }
+
+            return (x, y);
+        }
+
+        public void PrintSolution2()
+        {
+            var (x, y) = RunCommandsPart2(ReadCommands(ReadCommandInput()));
+            Console.WriteLine(x * y);
+        }
+    }
+}
---
+++ Tests/Day2.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using Advent_of_Code_2021.Day_2;
+using Xunit;
+
+namespace TestProject1
+{
+    public class Day2Tests
+    {
+        [Fact]
+        public void TestAgainstExample()
+        {
+            string[] input =
+            {
+                "forward 5",
+                "down 5",
+                "forward 8",
+                "up 3",
+                "down 8",
+                "forward 2"
+            };
+
+            var parsedCommands = Day2.ReadCommands(input).ToArray();
+            var ranCommandsPart1 = Day2.RunCommandsPart1(parsedCommands);
+            
+            Assert.Equal((15, 10), ranCommandsPart1);
+            
+            
+            var ranCommandsPart2 = Day2.RunCommandsPart2(parsedCommands);
+            Assert.Equal((15, 60), ranCommandsPart2);
+        }
+    }
+}
